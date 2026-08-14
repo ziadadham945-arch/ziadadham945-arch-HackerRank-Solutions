42468c0b371dfeedc1632d5c634386621a06687b
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:46:36 +0300

    Force committed solution - Time: O(n log n), Space: O(n)

---
+++ 1374-find-the-runner-up-score-Hackerrank/1374-find-the-runner-up-score.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    print(sorted(list(set(arr)))[-2])
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
