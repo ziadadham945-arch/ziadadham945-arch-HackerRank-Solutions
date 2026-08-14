4295b24130d52bf33394458ae4e0d9ed5848f581
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:41:05 +0300

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 9870-s-wap-c-ase-Hackerrank/9870-s-wap-c-ase.py
@@ -0,0 +1,8 @@
+def swap_case(s):
+    return s.swapcase()
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
