9982d797f571316dd7f2ce04bf88835ce995d85a
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:28:01 +0300

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 11742-string-validators-Hackerrank/11742-string-validators.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    s = input()   
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
