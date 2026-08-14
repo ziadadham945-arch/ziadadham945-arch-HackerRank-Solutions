205e5423c91527d22186045848c191c321cfb6eb
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:34:35 +0300

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 11766-text-wrap-Hackerrank/11766-text-wrap.py
@@ -0,0 +1,9 @@
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
