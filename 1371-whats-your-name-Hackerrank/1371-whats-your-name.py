eb5e2de77c5d5b851759f196d7927b74781cf9af
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:37:07 +0300

    Force committed solution - Time: O(N), Space: O(N)

---
+++ 1371-whats-your-name-Hackerrank/1371-whats-your-name.py
@@ -0,0 +1,15 @@
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
