e654d710a67f96317cf1708bf5fd0d9cc167a32f
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:20:53 +0300

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 7876-say-hello-world-with-python-Hackerrank/7876-say-hello-world-with-python.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
