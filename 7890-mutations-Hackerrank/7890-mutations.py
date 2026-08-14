79029532d6decb522bc19b29824aa81641a50952
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:36:39 +0300

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 7890-mutations-Hackerrank/7890-mutations.py
@@ -0,0 +1,8 @@
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position+1:]
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
