0bab55e7fcd920a7cc50bf76c7293fd6cabe69e2
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:21:48 +0300

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 22447-python-if-else-Hackerrank/22447-python-if-else.py
@@ -0,0 +1,18 @@
+
+#!/bin/python3
+n = int(input())
+
+if n % 2 != 0:
+    print("Weird")
+elif 2 <= n <= 5:
+    print("Not Weird")
+elif 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
+    
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
