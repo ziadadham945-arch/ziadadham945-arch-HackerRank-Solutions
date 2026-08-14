b8441d296b3bb82b151aee885b1eed824b08df2b
ziadadham945-arch <ziadadham945@gmail.com>
2026-08-14 17:45:46 +0300

    Force committed solution - Time: O(n log n), Space: O(n)

---
+++ 1375-nested-lists-Hackerrank/1375-nested-lists.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+    
+    second_lowest = sorted(list(set([score for name, score in students])))[1]
+    
+    for name in sorted([name for name, score in students if score == second_lowest]):
+        print(name)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
